bfe4674bb2cf3d13fa0d0ed012a00c519f3d2fdc
Max Ushakov <ushmax@gmail.com>
2008-06-21 11:01:22 +0400

    forgotten file added

---
+++ code/all.c
@@ -0,0 +1,14 @@
+#include "global.h"
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <string.h>
+#include <util/delay.h>
+
+#include "data.h"
+
+#include "all-static.h"
+#include "all-code.c"
